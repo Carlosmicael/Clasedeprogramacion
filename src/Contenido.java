591191a8ced9cb0b6c1c289571642328b19bb260
carlosmicael <netoblion2000@gmail.com>
2022-10-28 11:25:53 -0500

    first commit

---
+++ src/Contenido.java
@@ -0,0 +1,28 @@
+public class Contenido {
+    public static void main(String[] args) {
+        System.out.println("Múltiplos de byte");
+        System.out.println("1 KB");
+        System.out.println("1 MB");
+        System.out.println("1 GB");
+        System.out.println("1 TB");
+        System.out.println("1 PB");
+        System.out.println("1 EB");
+        System.out.println("1 ZB");
+        System.out.println("Convertir prefijos binarios");
+        System.out.println("Dividir arriba");
+        System.out.println("Multiplicar abajo");
+        System.out.println("Conversión decimal a binario");
+        System.out.println("Dividir el número decimal entre 2");
+        System.out.println("Bit y Byte");
+        System.out.println("Bit, es el elemento de dato más pequeño");
+        System.out.println("Byte, es el conjunto de 8 bits");
+        System.out.println("Sistema binario y decimal");
+        System.out.println("El sistema binario es un sistema de numeración");
+        System.out.println("El sistema decimal es un sitema de numeración");
+        System.out.println("El sistema binario es utilizado con el 1 y el 0");
+        System.out.println("El sistema decimal, es utilizado para los seres humanos");
+        System.out.println("Git");
+        System.out.println("Representación de datos en código binario");
+    }
+}
+
